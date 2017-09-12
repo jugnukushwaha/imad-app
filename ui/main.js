233e49a843ea2481340865d6098ce10b23afd42f
JUGNU KUMAR <jugnuk2@gmail.com>
2017-09-12 22:12:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,11 +7,11 @@ var img= document.getElementById('me');
 var marginleft=0;
 function moveright()
 {
-    marginleft=marginleft+10;
+    marginleft = marginleft + 10;
     img.style.marginleft=marginleft + 'px';
     
 }
 img.onclick = function()
 {
-var interval=setinterval(moveright,100); 
+var interval = setinterval(moveright,100); 
 };
