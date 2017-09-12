614b177c6b368105b26a56a7a2196a1a0d888c94
JUGNU KUMAR <jugnuk2@gmail.com>
2017-09-13 01:05:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 //change the text of the main text
-var element=document.getElementById('main-text');
-element.innerHTML='Hi,i have made some changes here...';
+//var element=document.getElementById('main-text');
+//element.innerHTML='Hi,i have made some changes here...';
 //move the image
 var img= document.getElementById('main-file');
 var marginLeft=0;
