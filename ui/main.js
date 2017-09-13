d62dc67e18cd329ac5b832606af27ae28940a68c
JUGNU KUMAR <jugnuk2@gmail.com>
2017-09-13 16:04:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,3 +15,12 @@ img.onclick = function()
 {
 var interval = setInterval(moveRight,30); 
 };
+var button=document.getElementById('counter');
+var counter =0;
+button.onclick=function()
+{
+    counter +=1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
+}
